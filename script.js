f4af0d7a84f3c2a2806aa7e7c66113fc62a84d2b
Pasquale de Sario <pasquale.desario0@gmail.com>
2025-11-10 16:34:41 +0100

    Mobile

--- script.js
+++ script.js
@@ -563,11 +563,3 @@ function shuffle(items) {
     return array;
 }
 
-function setVh() {
-  // 1% dell'altezza interna reale del viewport
-  const vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-}
-setVh();
-window.addEventListener('resize', setVh);
-window.addEventListener('orientationchange', setVh);
